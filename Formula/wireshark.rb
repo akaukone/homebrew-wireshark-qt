897a5935fd81597bfda4144e01bd72034ff33297
Antte Kaukonen <antte.kaukonen@ge.com>
2019-11-07 08:51:35 +0200

    Added wireshark.rb
    wireshark.rb from 615c7b87d8a8986c0d6212abddd2fa75de61f95a with commit
    6a81aeee836f93d805f679e4f77a1a9589fa35d5 reverted and --with-qt option
    permanently enabled.

---
+++ Formula/wireshark.rb
@@ -0,0 +1,81 @@
+class Wireshark < Formula
+  desc "Graphical network analyzer and capture tool"
+  homepage "https://www.wireshark.org"
+  url "https://www.wireshark.org/download/src/all-versions/wireshark-3.0.6.tar.xz"
+  mirror "https://1.eu.dl.wireshark.org/src/all-versions/wireshark-3.0.6.tar.xz"
+  sha256 "a87f4022a0c15ddbf1730bf1acafce9e75a4e657ce9fa494ceda0324c0c3e33e"
+  head "https://code.wireshark.org/review/wireshark", :using => :git
+
+  bottle do
+    sha256 "3c8cf1ded2ff308e559e7a99e4ee63cbbf75a2e056c16e9e24fe61cc6f20a1eb" => :catalina
+    sha256 "75b740d762545fe8df821c59ebde5a7dd3e55d2901d5594039195d4addfc9207" => :mojave
+    sha256 "76b08af389db2d83fc43bdd3bc6f2fbc78b755005676dbd586add801ee092a81" => :high_sierra
+  end
+
+  depends_on "cmake" => :build
+  depends_on "c-ares"
+  depends_on "glib"
+  depends_on "gnutls"
+  depends_on "libgcrypt"
+  depends_on "libmaxminddb"
+  depends_on "libsmi"
+  depends_on "libssh"
+  depends_on "lua@5.1"
+  depends_on "nghttp2"
+  depends_on "qt
+
+  def install
+    args = std_cmake_args + %W[
+      -DENABLE_CARES=ON
+      -DENABLE_GNUTLS=ON
+      -DENABLE_MAXMINDDB=ON
+      -DBUILD_wireshark_gtk=OFF
+      -DENABLE_PORTAUDIO=OFF
+      -DENABLE_LUA=ON
+      -DLUA_INCLUDE_DIR=#{Formula["lua@5.1"].opt_include}/lua-5.1
+      -DLUA_LIBRARY=#{Formula["lua@5.1"].opt_lib}/liblua5.1.dylib
+      -DCARES_INCLUDE_DIR=#{Formula["c-ares"].opt_include}
+      -DGCRYPT_INCLUDE_DIR=#{Formula["libgcrypt"].opt_include}
+      -DGNUTLS_INCLUDE_DIR=#{Formula["gnutls"].opt_include}
+      -DMAXMINDDB_INCLUDE_DIR=#{Formula["libmaxminddb"].opt_include}
+      -DENABLE_SMI=ON
+      -DBUILD_sshdump=ON
+      -DBUILD_ciscodump=ON
+      -DENABLE_NGHTTP2=ON
+      -DBUILD_wireshark=ON
+      -DENABLE_APPLICATION_BUNDLE=ON
+      -DENABLE_QT5=ON
+    ]
+
+    system "cmake", *args
+    system "make", "install"
+
+    prefix.install bin/"Wireshark.app"
+    bin.install_symlink prefix/"Wireshark.app/Contents/MacOS/Wireshark" => "wireshark"
+
+    # Install headers
+    (include/"wireshark").install Dir["*.h"]
+    (include/"wireshark/epan").install Dir["epan/*.h"]
+    (include/"wireshark/epan/crypt").install Dir["epan/crypt/*.h"]
+    (include/"wireshark/epan/dfilter").install Dir["epan/dfilter/*.h"]
+    (include/"wireshark/epan/dissectors").install Dir["epan/dissectors/*.h"]
+    (include/"wireshark/epan/ftypes").install Dir["epan/ftypes/*.h"]
+    (include/"wireshark/epan/wmem").install Dir["epan/wmem/*.h"]
+    (include/"wireshark/wiretap").install Dir["wiretap/*.h"]
+    (include/"wireshark/wsutil").install Dir["wsutil/*.h"]
+  end
+
+  def caveats; <<~EOS
+    If your list of available capture interfaces is empty
+    (default macOS behavior), install ChmodBPF:
+
+      brew cask install wireshark-chmodbpf
+  EOS
+  end
+
+  test do
+    system bin/"randpkt", "-b", "100", "-c", "2", "capture.pcap"
+    output = shell_output("#{bin}/capinfos -Tmc capture.pcap")
+    assert_equal "File name,Number of packets\ncapture.pcap,2\n", output
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# homebrew-wireshark-qt
-Wireshark formula with QT
+# Wireshark QT tap
+
+This repository contains Homebrew formula for
+[wireshark](https://www.wireshark.org/) with `--with-qt` option enabled.
+
+# Installing
+
+```
+$ brew tap akaukone/wireshark-qt
+$ brew install akaukone/wireshark-qt
+```
+
